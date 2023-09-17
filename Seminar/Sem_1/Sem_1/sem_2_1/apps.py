64934d5f97f7b89e6a9fb61c08acdf6aa47b62fe
Anna <109809860+annkrv80@users.noreply.github.com>
2023-09-17 17:13:28 +0700

    Delete Seminar directory

--- Seminar/Sem_1/Sem_1/sem_2_1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Sem21Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sem_2_1'
--- Seminar/Sem_1/Sem_1/sem_3_1_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Sem31AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sem_3_1_app'
